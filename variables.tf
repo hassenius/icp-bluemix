7cfeac4c9e54a100b49be0764c7cef58001e6f7a
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2017-10-11 12:58:35 +0100

    use terraform.tfvars

--- variables.tf
+++ variables.tf
@@ -10,3 +10,50 @@ variable "domain" { default = "ibmcloud.private" }
 
 # Name of the ICP installation, will be used as basename for VMs
 variable "instance_name" { default = "myicp" }
+
+##### ICP Instance details ######
+variable "master" {
+  type = "map"
+  
+  default = {
+    nodes       = "1"
+    cpu_cores   = "8"
+    disk_size   = "25" // GB
+    local_disk  = false
+    memory      = "8192"
+    network_speed= "1000"
+    private_network_only=false
+    hourly_billing=true
+  }
+ 
+}
+variable "proxy" {
+  type = "map"
+  
+  default = {
+    nodes       = "1"
+    cpu_cores   = "8"
+    disk_size   = "25" // GB
+    local_disk  = true
+    memory      = "8192"
+    network_speed= "1000"
+    private_network_only=false
+    hourly_billing=true
+  }
+ 
+}
+variable "worker" {
+  type = "map"
+  
+  default = {
+    nodes       = "2"
+    cpu_cores   = "8"
+    disk_size   = "25" // GB
+    local_disk  = true
+    memory      = "8192"
+    network_speed= "1000"
+    private_network_only=false
+    hourly_billing=true
+  }
+ 
+}
